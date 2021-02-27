3afc4b131f82353677e7385307133e2e0de54afd
Daniel Vogel <hello@dnlvgl.com>
2021-02-27 20:31:06 +0100

    move emoji to favicon

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ const pageColors = [
     { fg: "dark-blue", bg: "bg-washed-red" },
     { fg: "light-pink", bg: "bg-navy" }
 ];
-const titleEmojis = ["👋 ", "👨‍💻 ", "🙌 ", "🚀 ", "🎆 ", "🐦 "];
+const titleEmojis = ["👋", "👨‍💻", "🙌", "🚀", "🎆", "🐦"];
 const linkElems = document.querySelectorAll("a");
-const pageTitle = document.title;
+const favicon = document.querySelector("link[rel*='icon']");
 // save current color object and title emoji for random version
 let currentColor = null;
-let currentTitleEmoji = null;
+let currentFaviconEmoji = null;
 
 // get random Element from Array, used for colors and emojis
 const getRandomArraylElement = array => {
@@ -26,14 +26,14 @@ const getRandomColor = colors => {
 
 const getRandomEmoji = emojis => {
     // if we have a emoji set filter that to never have the same emoji twice
-    const filteredEmojis = emojis.filter(emoji => emoji !== currentTitleEmoji);
+    const filteredEmojis = emojis.filter(emoji => emoji !== currentFaviconEmoji);
     return getRandomArraylElement(filteredEmojis);
 }
 
 // select random color classes for body and link elements
 const selectTheme = () => {
     currentColor = getRandomColor(pageColors);
-    currentTitleEmoji = getRandomEmoji(titleEmojis);
+    currentFaviconEmoji = getRandomEmoji(titleEmojis);
     setTheme();
 };
 
@@ -42,7 +42,7 @@ const setTheme = () => {
     for (let element of linkElems) {
         element.classList.add(currentColor.fg);
     }
-    document.title = `${currentTitleEmoji} ${pageTitle}`;
+    favicon.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${currentFaviconEmoji}</text></svg>`
 }
 
 // reset theme by deleting classes on body and deleting fg color on all links
